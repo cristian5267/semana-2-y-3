476234a98d39dd4bc749c1b4b111b3961eb2238a
cristian5267 <cristianmartinez5167@gmail.com>
2025-05-21 21:39:15 -0500

    Actualizar modelo de productos

--- entity/products.entity.js
+++ entity/products.entity.js
@@ -7,6 +7,11 @@ export const Products = sequelize.define("products", {
         allowNull: false,
         defaultValue: "name",
     },
+    color: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        defaultValue: "color",
+    },
     price: {
         type: DataTypes.INTEGER,
         allowNull: false,
